8c7181647abe223087b7da6d7e896ca6335a2181
JL505vx <yisuselboss13@gmail.com>
2026-02-06 17:46:46 -0600

    Add demo doctor user creation from env

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,12 @@ set -e
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
+if [ "${DEMO_CREATE_DOCTOR}" = "true" ]; then
+  python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); u,created=User.objects.get_or_create(username='${DEMO_DOCTOR_USER}', defaults={'rol':'doctor','email':'${DEMO_DOCTOR_EMAIL}'}); 
+u.rol='doctor'; 
+u.is_active=True; 
+u.set_password('${DEMO_DOCTOR_PASSWORD}'); 
+u.save()"
+fi
+
 gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000}
